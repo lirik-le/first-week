6f3a1faeb84270d5333626af16c3bc863edcdccc
Kirill <lebedev.kirill258@gmail.com>
2024-01-13 21:01:14 +0700

    Третье задание

---
+++ third.ts
@@ -0,0 +1,10 @@
+// сделать палиндромы из строк "дим" "манек" "рота" чтоб получилось "димид" "манекенам" "ротатор"
+
+let words: string[] = ['дим', 'манек', 'рота'];
+
+for (let key in words) {
+    words[key] = words[key].concat(words[key].split('').reverse().splice(1).join(''));
+    console.log(words[key]);
+}
+
+console.log(words);
